a283f87225e4a3a3b76005556149b952a1eea028
Asim Khalil <108312289+Asimkhalil11@users.noreply.github.com>
2022-10-06 10:38:25 +0500

    Add files via upload

---
+++ 7NumberEight.js
@@ -0,0 +1,12 @@
+let addition = 4 + 4
+console.log("addition " + "4 " + "+ " + "4 " + "= " + addition)
+let subtraction = 10 - 2
+console.log("subtraction " + "10 " + "- " + "2 " + "= " + subtraction)
+let multiplication = 4 * 2
+console.log("multiplication " + "4 " + "* " + "2 " + "= " + multiplication)
+let division = 16 / 2 
+console.log("division " + "16 " + "/ " + "2 " + "= " + division)
+
+console.log("division " + "16 " + "/ " + "2 " + "= " + division)
+
+
